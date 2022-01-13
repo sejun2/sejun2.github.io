d4a9cd957cb270622bd78dfb22fb4ed0196fb069
Satoshi <satoshi@Satoshiui-MacBookAir.local>
2022-01-14 01:29:48 +0900

    stash

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5546b49d1fe8e98ebbe3a9a3150547ad",
-"index.html": "45d872672633418f833fea170bfc0fdc",
-"/": "45d872672633418f833fea170bfc0fdc",
-"main.dart.js": "08cb5078fc1f6db9e87911ff028ac6e1",
+"index.html": "f11760c8e2e5ff53e73a13fff111e32a",
+"/": "f11760c8e2e5ff53e73a13fff111e32a",
+"main.dart.js": "7a32ca00f80a49df739f20cc277865e4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
